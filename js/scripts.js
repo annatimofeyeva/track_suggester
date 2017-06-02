ef6bef2ee59c43583605e5d1360b9409c0250bfb
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-06-02 16:00:49 -0700

    add submit button

--- js/scripts.js
+++ js/scripts.js
@@ -23,16 +23,31 @@ $(document).ready(function() {
       $(".csharp_description").empty();
       $(".css_description").show();
     });
-
-//entering values in the form
-
-
-
-
-
-
-
+    
 
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+//creating variables with values reciving as users input from different input types: radio buttons, seach fields
+//var apps = $("input:radio[name=application]:checked").val();
+//$("div#output").text(apps);
+
+
+//$("input#person1").click(function(event) {
+//$("div#output").text();
+
+//var someInput = $("input#person1").val();
+//$("div#output").text(someInput);
+
+//"Enrique, you ought to study Java, so you can learn how to make mobile apps for Android!".
