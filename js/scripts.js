994da990a488cb9f6fb0a3092df6e4873d4ffd69
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-06-02 12:20:24 -0700

    html code completed

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,27 @@ $(document).ready(function() {
 $("span#ruby_rails").click(function() {
     $("span#ruby").show();
 });
-  ("span#php_drupal").click(function() {
+$("span#php_drupal").click(function() {
       $("span#php").show();
 });
-  ("span#java_android").click(function() {
+$("span#java_android").click(function() {
     $("span#java").show();
 });
-("span#css_design").click(function() {
+$("span#css_design").click(function() {
   $("span#css").show();
 });
-
-("span#c#_net").click(function() {
+$("span#cnet").click(function() {
   $("span#c-sharp").show();
 });
 
 
 
-
-
-
-
-
-
-
 });
+
+
+
+//class="btn btn-link"
+
+
+//$("#ruby").show();
+//$(".ruby_line").empty();
