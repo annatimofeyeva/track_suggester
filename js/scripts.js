d633d96e5a4e561d21ba3580365c44eb67fd5b04
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-06-03 15:36:09 -0700

    add new styling and jquery to panel-body buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,75 @@
+//stacks choise buttons with hide(), show() functions
 $(document).ready(function() {
-
   $("#ruby_text").click(function() {
-    $(".ruby_description").show();
+      $(".php_description").hide();
+      $(".java_description").hide();
+      $(".csharp_description").hide();
+      $(".css_description").hide();
+      $(".ruby_description").show();
    });
-
   $("#php_text").click(function(){
-    $(".ruby_description").empty();
-    $(".php_description").show();
+      $(".ruby_description").hide();
+      $(".csharp_description").hide();
+      $(".css_description").hide();
+      $(".php_description").show();
    });
-
   $("#java_text").click(function(){
-    $(".php_description").empty();
-    $(".java_description").show();
+      $(".php_description").hide();
+      $(".csharp_description").hide();
+      $(".css_description").hide();
+      $(".ruby_description").hide();
+      $(".java_description").show();
     });
-
-    $("#csharp_text").click(function(){
-      $(".java_description").empty();
+  $("#csharp_text").click(function(){
+      $(".php_description").hide();
+      $(".java_description").hide();
+      $(".ruby_description").hide();
+      $(".css_description").hide();
       $(".csharp_description").show();
     });
-
-    $("#css_text").click(function(){
-      $(".csharp_description").empty();
+  $("#css_text").click(function(){
+      $(".php_description").hide();
+      $(".java_description").hide();
+      $(".ruby_description").hide();
+      $(".csharp_description").hide();
       $(".css_description").show();
     });
-    
-
-
-
 });
+/*
+//code using functions that hides not needed descriptions
+$(document).ready(function() {
+  $("#ruby_text").click(function() {
+   showDescriptionFor(".ruby_description");
+   });
+  $("#php_text").click(function(){
+    showDescriptionFor(".php_description");
+   });
+  $("#java_text").click(function(){
+      showDescriptionFor(".java_description");
+    });
+  $("#csharp_text").click(function(){
+      showDescriptionFor(".csharp_description");
+    });
+  $("#css_text").click(function(){
+      showDescriptionFor(".css_description");
+    });
+//function that hides not needed descriptions
 
-
-
-
-
-
-
-
-
-
-
-//creating variables with values reciving as users input from different input types: radio buttons, seach fields
-//var apps = $("input:radio[name=application]:checked").val();
-//$("div#output").text(apps);
-
-
-//$("input#person1").click(function(event) {
-//$("div#output").text();
-
-//var someInput = $("input#person1").val();
-//$("div#output").text(someInput);
-
-//"Enrique, you ought to study Java, so you can learn how to make mobile apps for Android!".
+function showDescriptionFor(lang_name) {
+  var names = [
+    ".ruby_description",
+    ".java_description",
+    ".php_description",
+    ".csharp_description",
+    ".css_description"
+  ];
+  for (idx in names) {
+    if (names[idx] != lang_name) {
+      $(names[idx]).hide();
+    } else {
+      $(names[idx]).show();
+    }
+  }
+}
+});
+*/
